3cf9245b475db415230774828053b3e808c167f5
jingliancui <ericcui@qq.com>
2020-09-15 16:52:44 +0800

    update part one

--- SampleApp/SampleApp/SampleApp.Android/MainActivity.cs
+++ SampleApp/SampleApp/SampleApp.Android/MainActivity.cs
@@ -26,7 +26,6 @@ namespace SampleApp.Droid
 
             Xamarin.Forms.MessagingCenter.Subscribe<object>(this, MessagingCenterKey.Pay, obj =>
             {                
-
                 /*
 		         * 这里只是为了方便直接向商户展示支付宝的整个支付流程；所以Demo中加签过程直接放在客户端完成；
 		         * 真实App里，privateKey等数据严禁放在客户端，加签过程务必要放在服务端完成；
