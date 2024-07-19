276be5e8f3494c241206abb6c7f439ffc139bde4
Eric <ericcui@qq.com>
2024-07-19 23:41:23 +0800

    add MAUI sample app

---
+++ MauiSampleApp/SampleApp/SampleApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,33 @@
+﻿using AlipaySDK_ApiDefinition;
+using Foundation;
+using System.Diagnostics;
+using UIKit;
+
+namespace SampleApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public override bool OpenUrl(UIApplication application, NSUrl url, NSDictionary options)
+        {
+            AlipaySDK.DefaultService.ProcessOrderWithPaymentResult(url, resultDic => 
+            {
+                IDictionary<string, string> result = new Dictionary<string, string>();
+                resultDic.Keys?.ToList().ForEach(key =>
+                {
+                    var getOk = resultDic.TryGetValue(key, out NSObject obj);
+                    if (getOk)
+                    {
+                        result.Add(key.ToString(), obj.ToString());
+                    }
+
+                    Debug.WriteLine($"{result.Keys.FirstOrDefault()},{result.Values.FirstOrDefault()}");
+                });
+            });
+
+            return true;
+        }
+    }
+}
